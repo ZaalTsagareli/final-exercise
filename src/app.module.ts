003242bc4fa737e9433a5ced2d4b6b13b8a836ea
ZaalTsagareli <zaali.tsagareli@makingscience.com>
2023-10-10 14:26:45 +0400

    added database connection and env

--- src/app.module.ts
+++ src/app.module.ts
@@ -1,8 +1,19 @@
 import { Module } from '@nestjs/common';
 import { DoctorModule } from './doctor/doctor.module';
+import { ConfigModule, ConfigService } from '@nestjs/config';
+import { TypeOrmModule } from '@nestjs/typeorm';
+import { getDatabaseConfig } from 'libs/database';
 
 @Module({
-  imports: [DoctorModule],
+  imports: [
+    ConfigModule.forRoot({ isGlobal: true }),
+    DoctorModule,
+    TypeOrmModule.forRootAsync({
+      imports: [ConfigModule],
+      useFactory: getDatabaseConfig,
+      inject: [ConfigService],
+    }),
+  ],
   controllers: [],
   providers: [],
 })
